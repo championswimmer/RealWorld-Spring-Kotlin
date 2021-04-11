b988226686060b9d80822f2522af52d714c6a661
Arnav Gupta <arnav@codingblocks.com>
2021-04-11 08:58:35 +0530

    add first entity to save user
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/main/kotlin/tech/arnav/conduit/data/entities/BaseEntity.kt
+++ src/main/kotlin/tech/arnav/conduit/data/entities/BaseEntity.kt
@@ -1,4 +1,13 @@
 package tech.arnav.conduit.data.entities
 
-class BaseEntity {
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    var id: Long? = null
 }
--- src/main/kotlin/tech/arnav/conduit/data/entities/User.kt
+++ src/main/kotlin/tech/arnav/conduit/data/entities/User.kt
@@ -1,3 +1,15 @@
 package tech.arnav.conduit.data.entities
 
-data class User()
+import java.net.URL
+import javax.persistence.Column
+import javax.persistence.Entity
+
+@Entity(name = "users")
+data class User(
+    @Column(unique = true)
+    val username: String,
+    val email: String,
+    val bio: String,
+    val password: String,
+    val image: URL
+) : BaseEntity()
